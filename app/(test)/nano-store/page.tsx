d6c3d2691715fa7e68880ca8b5005c9d831afa00
longnguyen <longnguyenctt@gmail.com>
2024-04-17 17:13:13 +0700

    feat: implement history

--- app/(test)/nano-store/page.tsx
+++ app/(test)/nano-store/page.tsx
@@ -2,195 +2,8 @@
 import { DeepMapStore, atom, computed, deepMap, listenKeys } from 'nanostores';
 import { useStore } from '@nanostores/react';
 import { ReactNode, useEffect, useState } from 'react';
-
-const initialState = {
-  id: 'UDp7KNNtqoBFG_-VAgPHS',
-  name: 'New Design1',
-  sides: {
-    back: {
-      bgUrl:
-        'https://dev-minio-api.busycube.net/coolprint-static/brazil_back-1689766239261.webp',
-      objects: [
-        {
-          x: 194.5,
-          y: 200,
-          id: 'MfCP3sjO4m1YbssJB3y4E',
-          url: '',
-          fill: '#DBCAA7',
-          name: 'groupable',
-          text: 'Text here',
-          type: 'text',
-          align: 'left',
-          shape: 'normal',
-          width: 211,
-          height: 50,
-          scaleX: 1,
-          scaleY: 1,
-          opacity: 1,
-          visible: true,
-          fontSize: 50,
-          rotation: 0,
-          draggable: false,
-          fontStyle: 'bold',
-          resizable: true,
-          fontFamily: 'Roboto CoolPrint',
-          selectable: true,
-          showInExport: true,
-          letterSpacing: 0,
-          shapeStrength: 10,
-          fontFamilyName: '',
-          shadowSettings: null,
-          strokeSettings: null,
-        },
-      ],
-      previewUrl:
-        'https://dev-minio-api.busycube.net/coolprint-static/UDp7KNNtqoBFG_-VAgPHS_preview_back-1711526966635.png',
-      printableArea: {
-        size: {
-          width: 250,
-          height: 353.5,
-        },
-        position: {
-          x: 175,
-          y: 174.75,
-        },
-      },
-    },
-    front: {
-      bgUrl:
-        'https://dev-minio-api.busycube.net/coolprint-static/brazil_front_base-1689766239260.webp',
-      objects: [
-        {
-          x: 192.49999999999972,
-          y: 193.6811118084357,
-          id: 'UXsArOdtb9vACr-gq6C58',
-          url: 'https://dev-minio-api.busycube.net/coolprint-static/brazil_right_logo-1690547762698.png',
-          fill: '',
-          name: 'groupable',
-          type: 'img',
-          extra: {
-            name: 'left logo',
-            type: 'img',
-          },
-          width: 125,
-          height: 125,
-          scaleX: 0.4705681065668069,
-          scaleY: 0.47056810656680675,
-          opacity: 1,
-          visible: true,
-          fileName: 'Left Logo',
-          rotation: 0,
-          draggable: false,
-          resizable: true,
-          selectable: true,
-          uploadable: false,
-          showInExport: true,
-        },
-        {
-          x: 317.5894933395746,
-          y: 162.68111180843607,
-          id: 'tJBVtY1T_JABpnpm4SqFa',
-          url: 'https://dev-minio-api.busycube.net/coolprint-static/brazil_left_logo-1689766239260.png',
-          fill: '',
-          name: 'groupable',
-          type: 'img',
-          extra: {
-            name: 'right logo',
-            type: 'img',
-          },
-          width: 125,
-          height: 125,
-          scaleX: 0.850652751661124,
-          scaleY: 0.8506527516611233,
-          opacity: 1,
-          visible: true,
-          fileName: 'Right Logo',
-          rotation: 0,
-          draggable: false,
-          resizable: true,
-          selectable: true,
-          uploadable: false,
-          showInExport: true,
-        },
-        {
-          x: 194.5,
-          y: 288,
-          id: 'tsWYh-Mf2cvjCaie51YUE',
-          url: '',
-          fill: '#FFFFFF',
-          name: 'groupable',
-          text: 'Text here',
-          type: 'text',
-          align: 'left',
-          shape: 'normal',
-          width: 211,
-          height: 50,
-          scaleX: 1,
-          scaleY: 1,
-          opacity: 1,
-          visible: true,
-          fontSize: 50,
-          rotation: 0,
-          draggable: false,
-          fontStyle: 'bold',
-          resizable: true,
-          fontFamily: 'Roboto CoolPrint',
-          selectable: true,
-          showInExport: true,
-          letterSpacing: 0,
-          shapeStrength: 10,
-          fontFamilyName: '',
-          shadowSettings: null,
-          strokeSettings: null,
-        },
-        {
-          x: 262.5,
-          y: 357,
-          id: 'zg277XOI3jEpoBhnor9xO',
-          url: 'https://dev-minio-api.busycube.net/cdn/artwork/20230818/stickers/GotThis.svg',
-          fill: '',
-          name: 'groupable',
-          type: 'art',
-          extra: {
-            name: 'GotThis',
-            type: 'art',
-          },
-          width: 75,
-          height: 86,
-          scaleX: 1,
-          scaleY: 1,
-          opacity: 1,
-          visible: true,
-          fileName: 'GotThis',
-          rotation: 0,
-          draggable: false,
-          resizable: true,
-          selectable: true,
-          uploadable: false,
-          showInExport: true,
-        },
-      ],
-      previewUrl:
-        'https://dev-minio-api.busycube.net/coolprint-static/UDp7KNNtqoBFG_-VAgPHS_preview_front-1711526965784.png',
-      printableArea: {
-        size: {
-          width: 250,
-          height: 353.5,
-        },
-        position: {
-          x: 175,
-          y: 174.75,
-        },
-      },
-    },
-  },
-  width: 1200,
-  height: 1406,
-  customer: 'person',
-  template: '',
-  memberList: [],
-  centerTeamObjects: true,
-};
+import { initialState } from './data';
+import { create } from 'mutative';
 
 const state$ = deepMap(initialState);
 
@@ -234,43 +47,16 @@ type Store = DeepMapStore<typeof initialState>;
 const updateByKey = (store: Store) => (key: string, value: any) =>
   store.setKey(key as keyof typeof initialState, value);
 
-// const changeObjectPositionCommand: Command = (targetPath, value) => (store) => {
-//   const prevValue = getValueFromPath(store.value, targetPath);
-//   return {
-//     execute() {
-//       updateByKey(store)(targetPath, value);
-//     },
-//     undo() {
-//       updateByKey(store)(targetPath, prevValue);
-//     },
-//   };
-// };
-
-const changeTextCommand =
-  (targetPath: string, value: string) =>
-  (store: DeepMapStore<typeof initialState>) => {
-    const prevValue = getValueFromPath(store.value, targetPath);
-
-    return {
-      execute() {
-        state$.setKey(targetPath as keyof typeof initialState, value);
-      },
-      undo() {
-        state$.setKey(targetPath as keyof typeof initialState, prevValue);
-      },
-    };
-  };
-
 type Cb<T> = (_v: T) => any;
 
-const commandShell =
+const createCommand =
   ({
     getPrev,
     execution,
     undo,
   }: {
     getPrev: Cb<Store>;
-    execution: (s: Store) => void;
+    execution: Cb<Store>;
     undo: (s: Store, prevValue: any) => void;
   }) =>
   (store: Store) => {
@@ -286,8 +72,8 @@ const commandShell =
     };
   };
 
-const command1 = (newValue: any) =>
-  commandShell({
+const changeTextCommand = (newValue: any) =>
+  createCommand({
     getPrev(store) {
       return getValueFromPath(store.value, 'sides.front.objects[2].text');
     },
@@ -299,12 +85,74 @@ const command1 = (newValue: any) =>
     },
   });
 
+const PositionControl = () => {
+  const selectedObject = useStore(currentObjectSelector);
+
+  const [positionX, setPositionX] = useState(0);
+  const [positionY, setPositionY] = useState(0);
+
+  useEffect(() => {
+    if (!selectedObject) return;
+    setPositionX(selectedObject.x);
+    setPositionY(selectedObject.y);
+  }, [selectedObject]);
+
+  if (!selectedObject) return null;
+
+  const _command = (newValue: { x: number; y: number }) => {
+    return createCommand({
+      getPrev(store) {
+        return store.value;
+      },
+      execution: (store) => {
+        const newState = create(store.value, (draft) => {
+          if (!draft) return;
+          const idx = draft.sides.front.objects.findIndex(
+            (i) => i.id === selectedObject.id,
+          );
+          if (idx) {
+            draft.sides.front.objects[idx].x = newValue.x;
+            draft.sides.front.objects[idx].y = newValue.y;
+          }
+        });
+        if (!newState) return;
+        store.set(newState);
+      },
+      undo: (store, prevValue) => {
+        store.set(prevValue);
+      },
+    });
+  };
+
+  return (
+    <div>
+      <input
+        value={positionX}
+        onChange={(e) => setPositionX(+e.target.value)}
+      />
+      <input
+        value={positionY}
+        onChange={(e) => setPositionY(+e.target.value)}
+      />
+      <button
+        onClick={() =>
+          studioCommandManager.doCommand(
+            _command({ x: positionX, y: positionY }),
+          )
+        }
+      >
+        Update
+      </button>
+    </div>
+  );
+};
+
 const createCommandManager = (target: DeepMapStore<typeof initialState>) => {
   let history: any[] = [null];
   let position = 0;
 
   return {
-    doCommand(command: ReturnType<typeof changeTextCommand>) {
+    doCommand(command: any) {
       if (position < history.length - 1) {
         history = history.slice(0, position + 1);
       }
@@ -315,6 +163,7 @@ const createCommandManager = (target: DeepMapStore<typeof initialState>) => {
       position += 1;
 
       concreteCommand.execute();
+      console.log('history', history);
     },
 
     undo() {
@@ -332,13 +181,16 @@ const createCommandManager = (target: DeepMapStore<typeof initialState>) => {
     },
   };
 };
+
 const studioCommandManager = createCommandManager(state$);
 
-studioCommandManager.doCommand(command1('Long'));
+studioCommandManager.doCommand(changeTextCommand('Long'));
 
 // a.doCommand(changeTextCommand('sides.front.objects[2].text', 'Long'));
 
-const test = (id: string) => computed($currentObjectId, (s) => s === id);
+const isSelectedObject = (id: string) =>
+  computed($currentObjectId, (s) => s === id);
+
 const objectSelector = (id: string) =>
   computed(state$, (s) => s.sides.front.objects.find((i) => i.id === id));
 
@@ -347,7 +199,7 @@ const currentObjectSelector = computed([state$, $currentObjectId], (s, id) =>
 );
 
 const ObjectVisual = ({ id }: { id: string }) => {
-  const _isSelect = useStore(test(id));
+  const _isSelect = useStore(isSelectedObject(id));
   const object = useStore(objectSelector(id));
 
   if (!object) return null;
@@ -372,55 +224,12 @@ const Control = ({ children }: { children: ReactNode }) => {
   return <div>{children}</div>;
 };
 
-const registerCommand = (
-  commandKey: string,
-  execute: () => void,
-  undo: () => void,
-) => {};
-
-const ObjectAlignment = () => {
-  const object = useStore(currentObjectSelector);
-
-  const [position, setPosition] = useState({ x: object?.x, y: object?.y });
-
-  useEffect(() => {
-    setPosition({ x: object?.x, y: object?.y });
-  }, [object]);
-
-  const handleChangePosition = () => {
-    if (!object) return;
-    // studioCommandManager.doCommand({});
-    // studioCommandManager.doCommand(
-    //   changeObjectPositionCommand(
-    //     `sides.front.objects[${object.id}].x`,
-    //     position.x,
-    //   ),
-    // );
-    // studioCommandManager.doCommand(
-    //   changeObjectPositionCommand(
-    //     `sides.front.objects[${object.id}].y`,
-    //     position.y,
-    //   ),
-    // );
-  };
-
-  if (!object) return null;
-
-  return (
-    <div style={{ display: 'flex', gap: 20 }}>
-      <input value={position.x} />
-      <input value={position.y} />
-      <button onClick={handleChangePosition}>Update</button>
-    </div>
-  );
-};
-
 const $objectsKeyComputed = computed(state$, (state) =>
   state.sides.front.objects.map((i) => i.id),
 );
 
 const Page = () => {
-  const store = useStore(objectText);
+  const objText = useStore(objectText);
   const objectsKey = useStore($objectsKeyComputed);
 
   return (
@@ -428,23 +237,26 @@ const Page = () => {
       style={{ padding: 40, flexDirection: 'column', display: 'flex', gap: 30 }}
     >
       <input
-        value={store.text}
+        value={objText.text}
         onChange={(e) => {
-          studioCommandManager.doCommand(command1(e.target.value));
+          studioCommandManager.doCommand(changeTextCommand(e.target.value));
         }}
       />
+
       <div style={{ display: 'flex', gap: 40 }}>
         <button onClick={studioCommandManager.undo}>undo</button>
         <button onClick={studioCommandManager.redo}>redo</button>
       </div>
-      <ObjectAlignment />
+
+      <PositionControl />
 
       <div style={{ display: 'flex', gap: 20 }}>
         {objectsKey.map((id) => (
           <ObjectVisual key={id} id={id} />
         ))}
       </div>
-      <pre>{JSON.stringify(store, null, 4)}</pre>
+
+      <pre>{JSON.stringify(objText, null, 4)}</pre>
     </div>
   );
 };
